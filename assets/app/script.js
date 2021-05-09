a8ee56caa66d840f6d2b8b6358afd748ace413ad
Eslim Daga <esliim9811@gmail.com>
2021-05-09 10:07:59 -0500

    Change href locations

--- assets/app/script.js
+++ assets/app/script.js
@@ -31,19 +31,19 @@ function ingresar() {
   var rol = sessionStorage.getItem('rolUsuarioActivo');
   switch (rol) {
     case '1':
-      window.location.href = 'index.html';
+      window.location.href = 'home.html';
       break;
     case '2':
-      window.location.href = 'index.html';
+      window.location.href = 'home.html';
       break;
     case '3':
-      window.location.href = 'index.html';
+      window.location.href = 'home.html';
       break;
     case '4':
-      window.location.href = 'index.html';
+      window.location.href = 'home.html';
       break;
     default:
-      window.location.href = 'index.html';
+      window.location.href = 'home.html';
       break;
   }
 }
